88ff65b4f28b6b22eb23566adcd6101e9dfcb2bc
yoajeetyo <sajeetkumar40@gmail.com>
2021-08-22 23:21:29 +0530

    Added : Dice roll value

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,17 +1,22 @@
 import java.util.Random;
 
 public class SnakeLadder {
-	
-	int startPosition;
-public SnakeLadder(int value) {
-		super();
-		this.startPosition = value;
+
+	int value;
+
+	public SnakeLadder() {
+		this.value = 0;
 	}
-public static void main(String[] args) {
-		SnakeLadder snakeLadder = new SnakeLadder(0);
-		System.out.println("Start position of the player is : "+snakeLadder.startPosition);
-		
+
+	public int getDiceValue() {
+		double rand = Math.random();
+		this.value = (int) (1 + rand * 6);
+		return this.value;
 	}
-	
-	
+	public static void main(String[] args) {
+		SnakeLadder snakeLadder=new SnakeLadder();
+		System.out.println("Starting point of game is : "+snakeLadder.value);
+		System.out.println("Dice value is : "+snakeLadder.getDiceValue());
+	}
+
 }
