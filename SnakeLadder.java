02af84d03d5c72a3c9514c8a2f8045364c0a2b9c
yoajeetyo <sajeetkumar40@gmail.com>
2021-08-22 22:26:46 +0530

    Added : Start position of the player

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,6 +1,17 @@
-public class SnakeLadder {
-	public static void main(String[] args) {
-		System.out.println("Welcome to Snake Ladder game");
-	}
-}
+import java.util.Random;
 
+public class SnakeLadder {
+	
+	int startPosition;
+public SnakeLadder(int value) {
+		super();
+		this.startPosition = value;
+	}
+public static void main(String[] args) {
+		SnakeLadder snakeLadder = new SnakeLadder(0);
+		System.out.println("Start position of the player is : "+snakeLadder.startPosition);
+		
+	}
+	
+	
+}
