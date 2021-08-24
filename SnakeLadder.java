35759c8b1f2917d64f64e658d7867679cb5a45e4
yoajeetyo <sajeetkumar40@gmail.com>
2021-08-24 06:13:52 +0530

    Added : Repeat play until position is 100

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -2,7 +2,7 @@ import java.util.Random;
 
 public class SnakeLadder {
 	public static int start;
-	public static int initialPosition=0;
+	public static int initialPosition = 0;
 
 	public SnakeLadder() {
 		this.start = 0;
@@ -14,42 +14,43 @@ public class SnakeLadder {
 		return this.start;
 	}
 
-		
-
 	public static void main(String[] args) {
 		System.out.println("Welcome to game!!!");
 		SnakeLadder snakeLadder = new SnakeLadder();
 		System.out.println("Starting point of game is : " + initialPosition);
 		int diceValue = (int) snakeLadder.getDiceValue();
 		System.out.println("Dice value is : " + diceValue);
-		
-		int options = (int) (1+Math.random()*3);
-		switch (options) {
-		case 1:
-			System.out.println("you got Ladder");
-			initialPosition +=diceValue;
-			System.out.println("your value is now increased and current position is : "+initialPosition);
-			break;
-		case 2:
-			if(initialPosition>=start) {
-				System.out.println("you got snake");
-				initialPosition -=diceValue;
-				System.out.println("your value is decreased and your current position is : "+initialPosition);
+		while (initialPosition< 100) {
+			int options = (int) (1 + Math.random() * 3);
+			switch (options) {
+			case 1:
+				System.out.println("your dice value is : "+diceValue);
+				System.out.println("you got Ladder");
+				initialPosition += diceValue;
+				System.out.println("your value is now increased and current position is : " + initialPosition);
 				break;
-			}
-			else {
-				System.out.println("you got snake but you are at 0 position");
-				System.out.println("your remain at : "+initialPosition+" position");
-				break;
-			}
-			
+			case 2:
+				if (initialPosition >= start) {
+					System.out.println("your dice value is : "+diceValue);
+					System.out.println("you got snake");
+					initialPosition -= diceValue;
+					System.out.println("your value is decreased and your current position is : " + initialPosition);
+					break;
+				} else {
+					System.out.println("your dice value is : "+diceValue);
+					System.out.println("you got snake but you are at 0 position");
+					System.out.println("your remain at : " + initialPosition + " position");
+					break;
+				}
 
-		default:
-			System.out.println("you got no play");
-			System.out.println("your remain at : "+initialPosition+" position");
-			break;
+			default:
+				System.out.println("your dice value is : "+diceValue);
+				System.out.println(" But you got no play");
+				System.out.println("your remain at : " + initialPosition + " position");
+				break;
+			}
 		}
-		
+
 	}
 
 }
