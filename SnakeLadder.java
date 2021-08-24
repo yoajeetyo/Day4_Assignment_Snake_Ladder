bdb3e876ad3b98bcc3f4eb5efe411ed27f68cf49
yoajeetyo <sajeetkumar40@gmail.com>
2021-08-24 05:42:21 +0530

    Added : no play , ladder , snake

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,20 +1,55 @@
-public class SnakeLadder {
+import java.util.Random;
 
-	int value;
+public class SnakeLadder {
+	public static int start;
+	public static int initialPosition=0;
 
 	public SnakeLadder() {
-		this.value = 0;
+		this.start = 0;
 	}
 
 	public int getDiceValue() {
 		double rand = Math.random();
-		this.value = (int) (1 + rand * 6);
-		return this.value;
+		this.start = (int) (1 + rand * 6);
+		return this.start;
 	}
+
+		
+
 	public static void main(String[] args) {
-		SnakeLadder snakeLadder=new SnakeLadder();
-		System.out.println("Starting point of game is : "+snakeLadder.value);
-		System.out.println("Dice value is : "+snakeLadder.getDiceValue());
+		System.out.println("Welcome to game!!!");
+		SnakeLadder snakeLadder = new SnakeLadder();
+		System.out.println("Starting point of game is : " + initialPosition);
+		int diceValue = (int) snakeLadder.getDiceValue();
+		System.out.println("Dice value is : " + diceValue);
+		
+		int options = (int) (1+Math.random()*3);
+		switch (options) {
+		case 1:
+			System.out.println("you got Ladder");
+			initialPosition +=diceValue;
+			System.out.println("your value is now increased and current position is : "+initialPosition);
+			break;
+		case 2:
+			if(initialPosition>=start) {
+				System.out.println("you got snake");
+				initialPosition -=diceValue;
+				System.out.println("your value is decreased and your current position is : "+initialPosition);
+				break;
+			}
+			else {
+				System.out.println("you got snake but you are at 0 position");
+				System.out.println("your remain at : "+initialPosition+" position");
+				break;
+			}
+			
+
+		default:
+			System.out.println("you got no play");
+			System.out.println("your remain at : "+initialPosition+" position");
+			break;
+		}
+		
 	}
 
 }
